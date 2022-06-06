88e2adf30780cfd51878af55954a5f40430ea7e2
Rod Hale <guluarte@gmail.com>
2022-06-06 13:04:19 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,50 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React from 'react'
+import HeadComponent from '../components/Head'
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
-  
-  
   return (
-    <div className="App">
-      <HeadComponent/>
-      <div className="container">
-        <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+    <div className='App'>
+      <HeadComponent />
+      <div className='container'>
+        <header className='header-container'>
+          <p className='header'> 😳 Hale Emoji Store 😈</p>
+          <p className='sub-text'>
+            The only emoji store that accepts sh*tcoins
+          </p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          <div>
+            <iframe
+              allow='fullscreen'
+              frameBorder='0'
+              height='270'
+              src='https://giphy.com/embed/FlJ76bXyXwgWjOJkeJ/video'
+              width='480'
+            ></iframe>
+          </div>
         </main>
 
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+        <div className='footer-container'>
+          <img
+            alt='Twitter Logo'
+            className='twitter-logo'
+            src='twitter-logo.svg'
+          />
           <a
-            className="footer-text"
+            className='footer-text'
             href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
