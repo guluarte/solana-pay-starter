7badbaa9c5759fd2d2dd3d12ee27b7761a77b35c
Rod Hale <guluarte@gmail.com>
2022-06-11 14:19:15 -0600

    progress

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,47 @@
 import React from 'react'
-import HeadComponent from '../components/Head'
+import { PublicKey } from '@solana/web3.js'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 // Constants
 const TWITTER_HANDLE = '_buildspace'
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet()
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <div>
+        <iframe
+          allow='fullscreen'
+          frameBorder='0'
+          height='270'
+          src='https://giphy.com/embed/FlJ76bXyXwgWjOJkeJ/video'
+          width='480'
+        ></iframe>
+      </div>
+
+      <div className='button-container'>
+        <WalletMultiButton className='cta-button connect-wallet-button' />
+      </div>
+    </div>
+  )
+
   return (
     <div className='App'>
-      <HeadComponent />
       <div className='container'>
         <header className='header-container'>
           <p className='header'> 😳 Hale Emoji Store 😈</p>
           <p className='sub-text'>
-            The only emoji store that accepts sh*tcoins
+            The only emoji store that accepts shitcoins
           </p>
         </header>
 
         <main>
-          <div>
-            <iframe
-              allow='fullscreen'
-              frameBorder='0'
-              height='270'
-              src='https://giphy.com/embed/FlJ76bXyXwgWjOJkeJ/video'
-              width='480'
-            ></iframe>
-          </div>
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
 
         <div className='footer-container'>
