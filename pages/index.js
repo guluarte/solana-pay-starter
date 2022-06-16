5d0b3bdc5ea4b8d86dfde5f24b84bb20f75c56dc
Rodolfo Guluarte Hale <guluarte@gmail.com>
2022-06-16 13:17:27 -0600

    solana pay chapter 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Product from '../components/Product'
+
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import HeadComponent from '../components/Head'
 
 // Constants
@@ -6,6 +10,34 @@ const TWITTER_HANDLE = '_buildspace'
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const { publicKey } = useWallet()
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data)
+          console.log('Products', data)
+        })
+    }
+  }, [publicKey])
+
+  const renderNotConnectedContainer = () => (
+    <div className='button-container'>
+      <WalletMultiButton className='cta-button connect-wallet-button' />
+    </div>
+  )
+
+  const renderItemBuyContainer = () => (
+    <div className='products-container'>
+      {products.map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  )
+
   return (
     <div className='App'>
       <HeadComponent />
@@ -18,6 +50,7 @@ const App = () => {
         </header>
 
         <main>
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
           <div>
             <iframe
               allow='fullscreen'
